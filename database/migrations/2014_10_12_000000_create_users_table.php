5b5d38dd501f3b65fd1ce9253668c6d6e0b3ae55
Miguel Zdanovich <miguelangel.zdanovich@personal.com.ar>
2018-11-10 20:22:12 -0300

    se agrego Laravel Collective, UserTableSeeder y se configuro el userFactory

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('bio');
             $table->rememberToken();
             $table->timestamps();
         });
